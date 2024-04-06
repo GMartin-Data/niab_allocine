bb632f9443bedbd37d116d7ca08c285c33b45a09
GMartin-Data <gregory.martin.data@gmail.com>
2024-04-06 19:14:00 +0200

    Define BoxOfficePipeline

--- niab_ac/niab_ac/pipelines.py
+++ niab_ac/niab_ac/pipelines.py
@@ -6,8 +6,18 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from loguru import logger
 
 
-class NiabAcPipeline:
+class BoxOfficePipeline:
+    @logger.catch
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        film_id = adapter.get("film_id")
+        item["film_id"] = int(film_id)
+
+        entries = adapter.get("entries").replace(" ", "")
+        item["entries"] = int(entries)
+   
         return item
