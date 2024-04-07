75861dfa310b9f4dbc33ef05e1e60d50d425f6e3
GMartin-Data <gregory.martin.data@gmail.com>
2024-04-07 13:12:27 +0200

    Define FilmItem

--- niab_ac/niab_ac/items.py
+++ niab_ac/niab_ac/items.py
@@ -9,3 +9,19 @@ import scrapy
 class BoxOfficeItem(scrapy.Item):
     film_id = scrapy.Field()
     entries = scrapy.Field()
+
+
+class FilmItem(scrapy.Item):
+    film_id = scrapy.Field()
+    title = scrapy.Field()
+    img_src = scrapy.Field()
+    release = scrapy.Field()
+    duration = scrapy.Field()
+    genres = scrapy.Field()
+    synopsis = scrapy.Field()
+    nationality = scrapy.Field()
+    budget = scrapy.Field()
+    director = scrapy.Field()
+    casting = scrapy.Field()
+    societies = scrapy.Field()
+    entries = scrapy.Field()
